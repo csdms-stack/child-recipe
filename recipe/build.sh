19ed502f34de045bb36bb54a7d3393a85b3ee884
mcflugen <mcflugen@gmail.com>
2017-05-05 16:46:20 -0600

    Set optimization to O3.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ export CC=$(which gcc)
 export CXX=$(which g++)
 
 cd Child/Code && mkdir _build && cd _build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3"
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
