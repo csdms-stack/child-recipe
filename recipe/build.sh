df20a0a1f48d93609a0c3547c1416589f37daff0
mcflugen <mcflugen@gmail.com>
2017-05-05 16:30:08 -0600

    Build an optimized release version.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ export CC=$(which gcc)
 export CXX=$(which g++)
 
 cd Child/Code && mkdir _build && cd _build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
