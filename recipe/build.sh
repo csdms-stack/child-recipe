ca2b9251f38242f9c0cccc99548efe0ae42a414b
Mark Piper <mark.piper@colorado.edu>
2017-12-11 16:15:45 -0700

    Don't set CC and CXX
    conda-build sets them for us.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-export CC=$(which gcc)
-export CXX=$(which g++)
-
 cd Child/Code && mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3"
 make -j$CPU_COUNT all
