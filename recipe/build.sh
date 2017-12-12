eaf5bca1aafbeb8549a4c96742a71b46b44f68cc
mcflugen <mcflugen@gmail.com>
2017-12-12 16:27:49 -0700

    Compile with -std=c++98 only on osx.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-export CXXFLAGS="-std=c++98"
+if [ $(uname) == Darwin ]; then
+  export CXXFLAGS="-std=c++98"
+else
+  export CXXFLAGS="-std=c++11"
+fi
 
 cd Child/Code && mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="$CXXFLAGS -O3"
